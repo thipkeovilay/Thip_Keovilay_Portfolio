0d9a57dda0847c6d7be5ea42a2b71a051a19e30c
Thip <thip.keovilay@gmail.com>
2021-02-08 15:20:59 -0500

    checkkpoint

--- client/src/pages/Form.jsx
+++ client/src/pages/Form.jsx
@@ -2,13 +2,34 @@ import React, { useState } from "react";
 import "./Form.css";
 
 const Form = () => {
-  const state = {
-    step: 1,
-    name: "",
-    email: "",
-    phoneNumber: "",
-    message: "",
-  };
+  // const state = {
+  //   step: 1,
+  //   name: "",
+  //   email: "",
+  //   phoneNumber: "",
+  //   message: "",
+  // };
+
+  // //Proceed to next step
+  // nextStep = () => {
+  //   const { step } = this.state;
+  //   this.setState({
+  //     step: step + 1,
+  //   });
+  // };
+
+  // //Go back to prev step
+  // nextStep = () => {
+  //   const { step } = this.state;
+  //   this.setState({
+  //     step: step - 1,
+  //   });
+  // };
+
+  // //Handle fields change
+  // handleChange = (input) => (e) => {
+  //   this.setState({ [input]: e.target.value });
+  // };
 
   return <div></div>;
 };
