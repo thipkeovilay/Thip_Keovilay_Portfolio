79b8a67fb438db77f65bd7c00b50a5c3b0330bc3
Thip <thip.keovilay@gmail.com>
2021-02-08 15:25:47 -0500

    remove backend app

--- server.js
+++
@@ -1,8 +0,0 @@
-if (process.env.NODE_ENV !== "production") require("dotenv").config();
-
-const app = require("./server/app");
-const port = process.env.PORT || 8080;
-
-app.listen(port, () => {
-  console.log(`Express server is up on port ${port}`);
-});
